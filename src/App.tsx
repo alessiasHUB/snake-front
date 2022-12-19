95858d2babba395a92eca2af317c66aa0b85d592
Alessia B <borys.alessia@gmail.com>
2022-12-19 18:24:40 +0100

    added yellow apple

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 import useInterval from "./useInterval";
 import AppleRed from "./img/apple-red.png";
 import AppleGreen from "./img/apple-green.png";
-// import AppleYellow from "./img/apple-yellow.png";
+import AppleYellow from "./img/apple-yellow.png";
 import AppleBlue from "./img/apple-blue.png";
 
 const screenWidth = 1200;
@@ -47,7 +47,7 @@ function App() {
         if (eatenApples[0] === AppleRed) ctx.fillStyle = "#FF5100";
         else if (eatenApples[0] === AppleGreen) ctx.fillStyle = "#68FF00";
         else if (eatenApples[0] === AppleBlue) ctx.fillStyle = "#00DCFF";
-        // else if (eatenApples[0]===AppleYellow) ctx.fillStyle = "#FBFF00";
+        else if (eatenApples[0]===AppleYellow) ctx.fillStyle = "#FBFF00";
         snake.forEach(([x, y]) => ctx.fillRect(x, y, 1, 1));
         ctx.drawImage(fruit, apple[0], apple[1], 1, 1);
       }
@@ -72,20 +72,18 @@ function App() {
   function checkCollision(head: number[]) {
     // Green apple: snake can travel through walls
     if (eatenApples[0] === AppleGreen) {
-      for (let i = 0; i < head.length; i++) {
-        if (eatenApples[0] === AppleGreen) {
-          // if the snake goes through the left/right wall
-          if (head[0] < 0) {
-            head[0] = screenWidth / scale - 1;
-          } else if (head[0] * scale >= screenWidth) {
-            head[0] = 0;
-          }
-          // if the snake goes through the top/bottom wall
-          if (head[1] < 0) {
-            head[1] = screenHeight / scale - 1;
-          } else if (head[1] * scale >= screenHeight) {
-            head[1] = 0;
-          }
+      for (let i = 0; i < head.length; i++) {  
+        // if the snake goes through the left/right wall
+        if (head[0] < 0) {
+          head[0] = screenWidth / scale - 1;
+        } else if (head[0] * scale >= screenWidth) {
+          head[0] = 0;
+        }
+        // if the snake goes through the top/bottom wall
+        if (head[1] < 0) {
+          head[1] = screenHeight / scale - 1;
+        } else if (head[1] * scale >= screenHeight) {
+          head[1] = 0;
         }
       }
       // when the snake 'eat' itsels -> Game Over!
@@ -94,7 +92,6 @@ function App() {
       }
       return false;
     }
-
     // Blue apple: snake can go through itself
     if (eatenApples[0] === AppleBlue) {
       for (let i = 0; i < head.length; i++) {
@@ -107,7 +104,27 @@ function App() {
       }
       return false;
     }
-
+    // Yellow apple: snake can travel through walls & itself
+    if (eatenApples[0] === AppleYellow) {
+      for (let i = 0; i < head.length; i++) {
+        // if the snake goes through the left/right wall
+        if (head[0] < 0) {
+          head[0] = screenWidth / scale - 1;
+        } else if (head[0] * scale >= screenWidth) {
+          head[0] = 0;
+        }
+        // if the snake goes through the top/bottom wall
+        if (head[1] < 0) {
+          head[1] = screenHeight / scale - 1;
+        } else if (head[1] * scale >= screenHeight) {
+          head[1] = 0;
+        }
+      }
+      for (const s of snake) {
+        if (head[0] === s[0] && head[1] === s[1]) return false;
+      }
+      return false;
+    }
     // Red apple: snake cannot hit walls or itself
     if (eatenApples[0] === AppleRed) {
       for (let i = 0; i < head.length; i++) {
@@ -120,19 +137,6 @@ function App() {
       }
       return false;
     }
-
-    // Yellow apple: ???
-    // if (eatenApples[0] === AppleYellow){
-    //   for (let i = 0; i < head.length; i++) {
-    //     // snake hits wall -> Game Over!
-    //     if (head[i] < 0 || head[i] * scale >= screenWidth) return true;
-    //   }
-    //   for (const s of snake) {
-    //     // snake hits itself -> Game Over!
-    //     if (head[0] === s[0] && head[1] === s[1]) return true;
-    //   }
-    //   return false;
-    // }
   }
 
   function appleAte(newSnake: number[][]) {
@@ -144,17 +148,17 @@ function App() {
       setScore(score + 1);
       setApple(newApple);
 
-      // the red apple is most likely and blue least
-      // yellow is unused.
+      // the red apple is most likely, green, blue and yellow least
       let newAppleLogo = AppleRed;
       let num = Math.floor(Math.random() * 20);
       if (num > 10) {
+        if (num === 18) newAppleLogo = AppleYellow;
         num % 3 !== 0
           ? (newAppleLogo = AppleGreen)
           : (newAppleLogo = AppleBlue);
       }
       let color = "red";
-      if (newAppleLogo === AppleGreen) {
+      if (newAppleLogo === AppleGreen || newAppleLogo === AppleYellow) {
         color = "green";
       } else if (newAppleLogo === AppleBlue) {
         color = "blue";
